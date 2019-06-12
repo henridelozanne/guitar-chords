f903ff63ef1754e32fc030ac7b8e7eafe236c15a
Henri Delozanne <henri.delozanne@izifret.com>
2019-06-13 00:30:25 +0200

    Create smartphone-modal-menu.

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,17 +1,41 @@
 <template>
   <div class="menu">
-        <router-link class="no-decoration" to="/"><div class="menu-item menu-item-text">Search chords</div></router-link>
-        <router-link class="no-decoration" to="/quizz"><div class="menu-item menu-item-text" @click="goToQuizz">Quizz</div></router-link>
-        <router-link class="no-decoration" to="/about"><div class="menu-item menu-item-text">About</div></router-link>
-        <div class="hamburger menu-item">
-          <i class="el-icon-menu"></i>
-        </div>
-      </div>
+    <router-link class="no-decoration" to="/"><div class="menu-item menu-item-text">Search chords</div></router-link>
+    <router-link class="no-decoration" to="/quizz"><div class="menu-item menu-item-text">Quizz</div></router-link>
+    <router-link class="no-decoration" to="/about"><div class="menu-item menu-item-text">About</div></router-link>
+    <div class="hamburger menu-item" @click="toggleMenuModal">
+      <i class="el-icon-menu"></i>
+    </div>
+    <div class="modal" v-if="menuModalIsVisible">
+      <div class="modal-item search-chords-item" @click="goTo('/')">Search chords</div>
+      <div class="modal-item quizz-item" @click="goTo('/quizz')">Quizz</div>
+      <div class="modal-item about-item" @click="goTo('/about')">About</div>
+    </div>
+    <div v-if="menuModalIsVisible" @click="menuModalIsVisible=false" class="modal-close-icon-ctn">
+      X
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Menu',
+  data() {
+    return {
+      menuModalIsVisible: false,
+    };
+  },
+  methods: {
+    toggleMenuModal() {
+      this.menuModalIsVisible = !this.menuModalIsVisible;
+    },
+    goTo(page) {
+      if (this.menuModalIsVisible) {
+        this.menuModalIsVisible = false;
+      }
+      this.$router.push(page);
+    }
+  }
 }
 </script>
 
@@ -24,7 +48,57 @@ export default {
 
   .no-decoration {
     text-decoration: none;
-    color: white;
+    color: #ffffff;
+  }
+
+  .modal {
+    opacity: 0.9;
+    z-index: 2000;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgb(2, 2, 2);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .modal i {
+    background: rgb(194, 194, 194);
+    color: black;
+    cursor: pointer;
+    height: 50px;
+    flex-basis: 44px;
+    text-align: center;
+    line-height: 50px;
+  }
+
+  .modal-item {
+    border-top: rgb(242, 242, 242) solid 1px;
+    border-bottom: rgb(242, 242, 242) solid 1px;
+    padding: 30px;
+    text-align: center;
+    font-family: "Exo";
+    cursor: pointer;
+    min-width: 200px;
+    background: rgb(31, 31, 31);
+  }
+
+  .modal-close-icon-ctn {
+    position: absolute;
+    right: 30px;
+    width: 50px;
+    height: 50px;
+    z-index: 3000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(255, 255, 255, 0.2);
+    font-family: "Exo";
+    cursor: pointer;
   }
 
 </style>
