4274729fad22b2e1ab2f68ec9b9987a59fecafb1
Henri Delozanne <hnr.delozanne@gmail.com>
2020-09-01 16:46:25 +0200

    Add creator mention.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <div class="footer">
     <div class="footer-item">Partners</div>
     <div class="footer-item">Contact</div>
-    <div class="footer-item">Follow us on Facebook</div>
+    <div class="footer-item">Created by <a class="creator-link" target="_blank" href="https://www.henri-delozanne.com">Henri Delozanne</a></div>
   </div>
 </template>
 
@@ -14,16 +14,21 @@ export default {
 
 <style>
 
-  .footer {
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-    color: white;
-    font-size: 0.75em;
-  }
+.footer {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  color: white;
+  font-size: 0.75em;
+}
 
-  .footer-item {
-    cursor: pointer;
-  }
+.footer-item {
+  cursor: pointer;
+}
+
+.creator-link {
+  color: white;
+  text-decoration: none;
+}
 
 </style>
